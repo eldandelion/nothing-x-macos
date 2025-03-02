110a8f73a2cb8408fd548c30a787896f79ef233b
eldandelion <daniellane@qq.com>
2025-03-02 12:43:22 +0800

    Implementation for find my buds view

---
+++ Nothing X MacOS/domain/usecases/RingBudsUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  RingBudsUseCase.swift
+//  Nothing X MacOS
+//
+//  Created by Daniel on 2025/3/2.
+//
+
+import Foundation
---
+++ Nothing X MacOS/presentation/models/FindMyBudsViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FindMyBudsViewViewModel.swift
+//  Nothing X MacOS
+//
+//  Created by Daniel on 2025/3/2.
+//
+
+import Foundation
